c0ecc50c5370bf69862d1e179ea42b1d00bcddc8
carlos.avellar <carlos.avellar@gmail.com>
2023-03-02 19:21:17 -0300

    Almost

---
+++ src/components/App/index.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Login from './../Login/';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import logo from './../../assets/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" src={logo} alt="The Movies API" />
+      </header>
+      <div className="App-body">
+        {' '}
+        <Container>
+          <Row>
+            <Col>
+              <h1>
+                Login
+                {/* <code>src/App.js</code> and save to reload. */}
+              </h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col></Col>
+            <Col>
+              <Login />
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login/index.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import './Login.css';
+import useInput from './../../hooks/use-input';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function Login() {
+  const {
+    value: enteredEmail,
+    isValid: emailIsValid,
+    hasError: emailInputError,
+    valueChangeHandler: emailChangedHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput((value) => value.trim() !== '' && value.lenght < 6);
+
+  let isFormValid = false;
+  if (emailIsValid) {
+    isFormValid = true;
+  } else {
+    isFormValid = false;
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!emailIsValid) {
+      return;
+    }
+
+    resetEmail();
+  };
+
+  const emailInputClassname = emailIsValid ? 'mb-3 Login-invalid' : 'mb-3';
+
+  return (
+    <Form className="Login-body" onSubmit={submitHandler}>
+      <Form.Group className={emailInputClassname} controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="enteredEmail"
+          placeholder="Enter enteredEmail"
+          onChange={emailChangedHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
+        <Form.Text className="text-muted">{emailInputError && 'Email is invalid'} </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="enteredPassword" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Keep me logged in" />
+      </Form.Group>
+      <Button disabled={!isFormValid} className="Login-btn" variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default Login;
